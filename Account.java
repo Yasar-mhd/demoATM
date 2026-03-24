e71cfc94c9c1671d8910337b6cdbf13480658168
Yasar-mhd <yasarmahmood7x@gmail.com>
2026-03-25 00:29:24 +0530

    Created Account.java for implementing OOPS

---
+++ Account.java
@@ -0,0 +1,10 @@
+public class Account {
+    String name;
+    long balance;
+    int pin;
+    public Account(String name,long balance,int pin){
+        this.name=name;
+        this.balance=balance;
+        this.pin=pin;
+    }
+}
